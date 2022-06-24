e6cb5d159850bc039e0513623dd74b4f3b8780a8
Brandon Aray <alex.aray94@gmail.com>
2022-06-24 14:50:35 -0500

    Added functionality to inputs as well as showing error messages

--- src/components/CalculatorCard/index.jsx
+++ src/components/CalculatorCard/index.jsx
@@ -1,15 +1,29 @@
 import "./calculatorCard.css";
+import { useState } from "react";
 import Input from "../Input";
 import SelectTip from "../SelectTip";
 import TipResult from "../TipResult";
 
 export default function CalculatorCard() {
+  const [bill, setBill] = useState(0);
+  const [people, setPeople] = useState(0);
+  const [tip, setTip] = useState(0);
+  const [tipAmount, setTipAmount] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  console.log({ bill, people });
+
   return (
     <main className="main__card">
       <div className="main__left-container--desktop">
-        <Input name="Bill" type="number" icon="dollar" />
+        <Input name="Bill" type="number" icon="dollar" setFunc={setBill} />
         <SelectTip />
-        <Input name="Number of People" type="number" icon="person" />
+        <Input
+          name="Number of People"
+          type="number"
+          setFunc={setPeople}
+          icon="person"
+        />
       </div>
       <TipResult />
     </main>
--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,4 +1,5 @@
 import "./input.css";
+import { useRef, useState } from "react";
 import Dollar from "/src/images/Dollar.jsx";
 import Person from "/src/images/Person.jsx";
 
@@ -7,14 +8,31 @@ const ICONS = {
   person: <Person />,
 };
 
-export default function Input({ name, type, icon }) {
+export default function Input({ name, type, icon, setFunc }) {
+  const [error, setError] = useState(false);
+  const inputRef = useRef();
+
+  const handleChange = () => {
+    const input = Number(inputRef.current.value);
+    setFunc(input);
+    if (input <= 0) setError(true);
+    else setError(false);
+  };
+
   return (
     <div className="main__input-container">
       <div className="input__icon-container">{ICONS[icon]}</div>
       <label htmlFor={name} className="input__label">
         {name}
       </label>
-      <input type={type} id={name} className="main__input" />
+      <input
+        type={type}
+        id={name}
+        className={`main__input ${error ? "input__warning" : ""}`}
+        onChange={handleChange}
+        ref={inputRef}
+      />
+      {error && <span className="input__warning-msg">Can't be zero</span>}
     </div>
   );
 }
