49d8cca6193e1089442f37906c9830b7a26f90a1
Brandon Aray <alex.aray94@gmail.com>
2022-06-23 21:17:10 -0500

    Finished desktop responsive layout, no functionality

--- src/components/CalculatorCard/index.jsx
+++ src/components/CalculatorCard/index.jsx
@@ -6,9 +6,11 @@ import TipResult from "../TipResult";
 export default function CalculatorCard() {
   return (
     <main className="main__card">
-      <Input name="Bill" type="number" icon="dollar" />
-      <SelectTip />
-      <Input name="Number of People" type="number" icon="person" />
+      <div className="main__left-container--desktop">
+        <Input name="Bill" type="number" icon="dollar" />
+        <SelectTip />
+        <Input name="Number of People" type="number" icon="person" />
+      </div>
       <TipResult />
     </main>
   );
--- src/components/TipResult/index.jsx
+++ src/components/TipResult/index.jsx
@@ -3,18 +3,21 @@ import "./tipResult.css";
 export default function TipResult() {
   return (
     <div className="main__result">
-      <div className="main__result-top">
-        <p className="result__left">
-          Tip Amount <br /> <span className="result__gray-text">/ person</span>
-        </p>
-        <span className="result__right">$0</span>
-      </div>
-      <div className="main__result-bottom">
-        <p className="result__left">
-          Total <br />
-          <span className="result__gray-text">/ person</span>
-        </p>
-        <span className="result__right">$0</span>
+      <div>
+        <div className="main__result-top">
+          <p className="result__left">
+            Tip Amount <br />
+            <span className="result__gray-text">/ person</span>
+          </p>
+          <span className="result__right">$0</span>
+        </div>
+        <div className="main__result-bottom">
+          <p className="result__left">
+            Total <br />
+            <span className="result__gray-text">/ person</span>
+          </p>
+          <span className="result__right">$0</span>
+        </div>
       </div>
       <button className="result__btn">Reset</button>
     </div>
